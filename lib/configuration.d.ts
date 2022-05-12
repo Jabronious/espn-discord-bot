117972828ba8f9086ed9fe52db637db076136fb6
Jarrod Merryman <jarrodmerryman@jarrods-air.lan>
2022-05-11 21:04:52 -0500

    removes lib from source control

--- lib/configuration.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const configs: Readonly<{
-    FBB_BOT_SECRET: string;
-    FBB_BOT_CLIENT_ID: string;
-    FBB_BOT_TOKEN: string;
-    NODE_ENV: "development" | "test" | "production" | "staging";
-} & import("envalid").CleanedEnvAccessors>;
