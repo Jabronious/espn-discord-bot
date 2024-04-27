9e1bc41346301fcfa5b587395e6e195e84473678
Jarrod Merryman <jarrodmerryman@gmail.com>
2024-04-26 23:37:01 -0600

    remove k8s dir and add subdomain env var

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,9 +1,20 @@
 import { cleanEnv, str } from 'envalid';
 
+function subdomain() {
+	if (process.env.NODE_ENV === 'staging') {
+		return 'staging.';
+	} else if (process.env.NODE_ENV === 'production') {
+		return '';
+	} else {
+		return 'develop.';
+	}
+}
+
 export const configs = cleanEnv(process.env, {
 	FBB_BOT_SECRET: str(),
 	FBB_BOT_CLIENT_ID: str(),
 	FBB_BOT_TOKEN: str(),
 	COSMOS_CONNECTION_STRING: str({ devDefault: 'change-me' }),
 	NODE_ENV: str({ choices: ['local', 'development', 'test', 'production', 'staging'], default: 'development' }),
+	SUBDOMAIN: str({ default: subdomain() }),
 });
