5545cee64db12d94bbe38954696991800399404e
Jarrod Merryman <Jabronious@users.noreply.github.com>
2024-04-28 22:51:02 -0600

    Revert "Add env specific endpoints"

--- src/commands/connect.ts
+++ src/commands/connect.ts
@@ -1,18 +1,16 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
 import { UsersService } from '../services/user.service';
-import { configs } from '../configuration';
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('connect')
 		.setDescription('Direct you to the website to connect your fantasy account and discord'),
 	async execute(interaction: ChatInputCommandInteraction) {
-		const url = `https://www.${configs.SUBDOMAIN}efit-web.site`;
 		const userService = new UsersService();
 		const user = await userService.findByDiscordId(interaction.user.id);
-		let msg = `Connect your fantasy account to your discord here: ${url}`;
+		let msg = 'Connect your fantasy account to your discord here: https://www.efit-web.site';
 		if (user) {
-			msg = `You have already connected your accounts: ${url}`;
+			msg = 'You have already connected your accounts: https://www.efit-web.site';
 		}
 		await interaction.reply({
 			content: msg,
